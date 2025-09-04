c6d5a63754ec25b5bcf06c6a4e3fc78c772c497f
MaviUK <141088900+MaviUK@users.noreply.github.com>
2025-09-04 23:00:47 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,17 @@
+// tailwind.config.js
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
+  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg:      "#000000",
+        ink:     "#FBEBC3",
+        primary: "#082130",
+        secondary:"#264E28",
+        accent:  "#D58B3B",
+        muted:   "#58502B",
+      }
+    }
+  },
   plugins: [],
 }
